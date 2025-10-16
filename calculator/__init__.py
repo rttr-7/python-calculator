29d9dc691845c4b206ac7cf64b4ab52fc4a75ccd
Amay Chivate <amaychivate07@gmail.com>
2025-10-16 15:34:57 +0530

    feat: add power function for exponentiation

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,3 @@
-"""Calculator package."""
-from calculator.calculator import add, subtract, multiply, divide
+from calculator.calculator import add, subtract, multiply, divide, power
 
-__version__ = "0.0.0"
-__all__ = ["add", "subtract", "multiply", "divide"]
+__all__ = ["add", "subtract", "multiply", "divide", "power"]
